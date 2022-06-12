5e01599378f8c44a891fe427307d1e1b2b6d27b8
nbyrd <nbyrd@student.21-school.ru>
2022-06-12 14:51:59 +0300

    db

--- db.py
+++ db.py
@@ -1,9 +1,9 @@
 import psycopg2
 
-conn = psycopg2.connect(host='127.0.0.1', port=5432, dbname='hxh', user = 'zafar', password='12344321')
-cursor = conn.cursor()
-cursor.execute("SELECT user_id FROM users WHERE telegram_id=(%s)", ("1000",))
-print(cursor.fetchone()[0])
+# conn = psycopg2.connect(host='127.0.0.1', port=5432, dbname='hxh', user = 'zafar', password='12344321')
+# cursor = conn.cursor()
+# cursor.execute("INSERT INTO users(telegram_id) VALUES (%s)", ("2000",))
+# conn.commit()
 
 class BotDB:
 	def __init__(self, db_file) -> None:
@@ -18,17 +18,18 @@ class BotDB:
 		self.cursor.execute("SELECT user_id FROM users WHERE telegram_id=(%s)", (str(telegram_id),))
 		return self.cursor.fetchone()[0]
 
-# 	def add_user(self, telegram_id):
-# 		self.cursor.execute("INSERT INTO users(telegram_id) VALUES (%s)", (telegram_id))
-# 		return self.conn.commit()
+	def add_user(self, telegram_id):
+		self.cursor.execute("INSERT INTO users(telegram_id) VALUES (%s)", (str(telegram_id),))
+		return self.conn.commit()
 
-# 	def add_record(self, telegram_id, proff, vacancy):
-# 		self.cursor.execute("INSERT INTO records(telegram_id, proff, vanacy) VALUES (%s, %s, %s)",
-# 		(self.get_user_id(telegram_id), proff, vacancy))
+	def add_record(self, telegram_id, proff, vacancy):
+		self.cursor.execute("INSERT INTO records(telegram_id, proff, vanacy) VALUES (%s, %s, %s)",
+		(self.get_user_id(telegram_id), proff, vacancy))
+		return self.conn.commit()
 	
-# 	def get_records(self, telegram_id):
-# 		result = self.cursor.execute("SELECT * FROM records WHERE telegram_id (%s)", (telegram_id))
-# 		return result
+	def get_records(self, telegram_id):
+		self.cursor.execute("SELECT * FROM records WHERE telegram_id (%s)", (telegram_id))
+		return self.cursor.fetchall()
 
-# 	def close(self):
-# 		self.conn.close()
+	def close(self):
+		self.conn.close()
