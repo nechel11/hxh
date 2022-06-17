6e95ba31f30e3e44778068fe8009123a74b073f0
nbyrd <zaf-khasanov@yandex.ru>
2022-06-17 18:07:53 +0300

    go_part

--- web_backend(golang)/1.go
+++ web_backend(golang)/1.go
@@ -1,42 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
-	"database/sql"
-	"github.com/jackc/pgx/v4"
 )
 
-type User struct {
-	Name string
-	Age uint16
-	Money int16
-	Avg_grades, Happiness float64
-	Hobbies []string
+
+func index(w http.ResponseWriter, r *http.Request){
+	tmpl, err := template.ParseFiles("templates/index.html", "templates/header.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	tmpl.ExecuteTemplate(w, "index", nil)
 }
 
-type Contacts struct {
-	Name string
-	Telegram string
-	Hh_id string
-}
-
-
-func home_page(w http.ResponseWriter, r *http.Request){
-	bob := User{"Bob", 25, 10, 3.7, 0.8, []string{"footbal", "Skate"}}
-	tmpl, _ := template.ParseFiles("templates/home_page.html")
-	tmpl.Execute(w, bob)
-}
-
-func contacts_page(w http.ResponseWriter, r *http.Request){
-	contacts := Contacts{"Zafar Khasanov", "https://t.me/nechel1233", "https://hh.ru/resume/fe3d9308ff0727aa460039ed1f336b6276536e"}
-	tmpl, _ := template.ParseFiles("templates/contacts.html")
-	tmpl.Execute(w, contacts)
+func contacts(w http.ResponseWriter, r *http.Request){
+	tmpl, err := template.ParseFiles("templates/contacts.html", "templates/header.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	tmpl.ExecuteTemplate(w, "contacts", nil)
 }
 
 func handleRequest(){
-	http.HandleFunc("/", home_page)
-	http.HandleFunc("/contacts/", contacts_page)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/contacts/", contacts)
 	http.ListenAndServe(":8080", nil)
 }
 
