8c9397addd101a16520ce0a03f1b929646b9d19e
nbyrd <zaf-khasanov@yandex.ru>
2022-06-18 13:33:46 +0300

    go_part

--- web_backend(golang)/1.go
+++ web_backend(golang)/1.go
@@ -15,6 +15,14 @@ func index(w http.ResponseWriter, r *http.Request){
 	tmpl.ExecuteTemplate(w, "index", nil)
 }
 
+func create(w http.ResponseWriter, r *http.Request){
+	tmpl, err := template.ParseFiles("templates/create.html", "templates/header.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	tmpl.ExecuteTemplate(w, "create", nil)
+}
+
 func contacts(w http.ResponseWriter, r *http.Request){
 	tmpl, err := template.ParseFiles("templates/contacts.html", "templates/header.html")
 	if err != nil {
@@ -23,13 +31,20 @@ func contacts(w http.ResponseWriter, r *http.Request){
 	tmpl.ExecuteTemplate(w, "contacts", nil)
 }
 
+func save_article(w http.ResponseWriter, r *http.Request){
+	title := r.FormValue("title")
+	anons := r.FormValue("anons")
+	full_text := r.FormValue("full_text")
+}
+
+
 func handleRequest(){
 	http.HandleFunc("/", index)
 	http.HandleFunc("/contacts/", contacts)
+	http.HandleFunc("/create/", create)
 	http.ListenAndServe(":8080", nil)
 }
 
 func main(){
-	
 	handleRequest()
 }
